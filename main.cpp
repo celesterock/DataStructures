ed36a1f8c038a4040ed2878eceb830533f8244ca
celesterock <77873309+celesterock@users.noreply.github.com>
2024-01-22 16:21:28 -0800

    Creation of main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// Author: Celeste Rock
+// Description: Hello World in C++
+//============================================================
+main
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  string name1="Celeste Rock";
+  return 0;
+}
